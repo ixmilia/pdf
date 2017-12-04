f9a0ecb919b329413abee0b6dd91af5c70725b18
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-12-04 15:46:58 -0800

    add helpers for unit conversions

---
+++ src/IxMilia.Pdf.Test/PdfMeasurementTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using Xunit;
+
+namespace IxMilia.Pdf.Test
+{
+    public class PdfMeasurementTests
+    {
+        [Fact]
+        public void RawPointsNoopTest()
+        {
+            var m = PdfMeasurement.Points(42.0);
+            Assert.Equal(42.0, m.AsPoints());
+        }
+
+        [Fact]
+        public void ConvertTest()
+        {
+            var oneInch = PdfMeasurement.Inches(1.0);
+            Assert.Equal(72.0, oneInch.AsPoints());
+            Assert.Equal(25.4, oneInch.ConvertTo(PdfMeasurementType.Mm).RawValue);
+        }
+
+        [Fact]
+        public void EqualityTest()
+        {
+            var oneInch1 = PdfMeasurement.Inches(1.0);
+            var oneInch2 = PdfMeasurement.Mm(25.4);
+            Assert.Equal(oneInch1, oneInch2);
+        }
+
+        [Fact]
+        public void BinaryOperatorTest()
+        {
+            var oneInch1 = PdfMeasurement.Inches(1.0);
+            var oneInch2 = PdfMeasurement.Mm(25.4);
+            Assert.Equal(2.0, (oneInch1 + oneInch2).RawValue);
+            Assert.Equal(50.8, (oneInch2 + oneInch1).RawValue);
+        }
+    }
+}
---
+++ src/IxMilia.Pdf/PdfMeasurement.cs
@@ -0,0 +1,123 @@
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+
+namespace IxMilia.Pdf
+{
+    public enum PdfMeasurementType
+    {
+        Point = 0,
+        Inch = 1,
+        Mm = 2,
+    }
+
+    public struct PdfMeasurement : IEquatable<PdfMeasurement>
+    {
+        private const double PointsPerInch = 72.0;
+        private const double PointsPerMm = PointsPerInch / 25.4;
+
+        public double RawValue { get; }
+        public PdfMeasurementType MeasurementType { get; }
+
+        public PdfMeasurement(double value, PdfMeasurementType measurementType)
+            : this()
+        {
+            RawValue = value;
+            MeasurementType = measurementType;
+        }
+
+        public double AsPoints()
+        {
+            return ConvertTo(PdfMeasurementType.Point).RawValue;
+        }
+
+        public PdfMeasurement ConvertTo(PdfMeasurementType targetMeasurementType)
+        {
+            var toPointsScale = GetConverterScale(MeasurementType);
+            var toTargetScale = GetConverterScale(targetMeasurementType);
+            var scale = toPointsScale / toTargetScale;
+            var newValue = RawValue * scale;
+            return new PdfMeasurement(newValue, targetMeasurementType);
+        }
+
+        public static bool operator ==(PdfMeasurement a, PdfMeasurement b)
+        {
+            return a.AsPoints() == b.AsPoints();
+        }
+
+        public static bool operator !=(PdfMeasurement a, PdfMeasurement b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is PdfMeasurement))
+            {
+                return false;
+            }
+
+            return Equals((PdfMeasurement)obj);
+        }
+
+        public bool Equals(PdfMeasurement other)
+        {
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return AsPoints().GetHashCode();
+        }
+
+        public static PdfMeasurement operator +(PdfMeasurement a, PdfMeasurement b)
+        {
+            return new PdfMeasurement(a.RawValue + b.ConvertTo(a.MeasurementType).RawValue, a.MeasurementType);
+        }
+
+        public static PdfMeasurement operator -(PdfMeasurement a, PdfMeasurement b)
+        {
+            return new PdfMeasurement(a.RawValue - b.ConvertTo(a.MeasurementType).RawValue, a.MeasurementType);
+        }
+
+        public static PdfMeasurement operator *(PdfMeasurement a, double b)
+        {
+            return new PdfMeasurement(a.RawValue * b, a.MeasurementType);
+        }
+
+        public static PdfMeasurement operator /(PdfMeasurement a, double b)
+        {
+            return new PdfMeasurement(a.RawValue / b, a.MeasurementType);
+        }
+
+        private static double GetConverterScale(PdfMeasurementType measurementType)
+        {
+            switch (measurementType)
+            {
+                case PdfMeasurementType.Point:
+                    return 1.0;
+                case PdfMeasurementType.Inch:
+                    return PointsPerInch;
+                case PdfMeasurementType.Mm:
+                    return PointsPerMm;
+                default:
+                    throw new InvalidOperationException($"Unexpected measurement type '{measurementType}'.");
+            }
+        }
+
+        public static PdfMeasurement Points(double value)
+        {
+            return new PdfMeasurement(value, PdfMeasurementType.Point);
+        }
+
+        public static PdfMeasurement Inches(double value)
+        {
+            return new PdfMeasurement(value, PdfMeasurementType.Inch);
+        }
+
+        public static PdfMeasurement Mm(double value)
+        {
+            return new PdfMeasurement(value, PdfMeasurementType.Mm);
+        }
+    }
+}
