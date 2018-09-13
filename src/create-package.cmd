f0e63ab500effff7cc56888c10abbaedb29701ea
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-09-13 16:35:13 -0700

    let SourceLink handle the commit hash embedding

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -3,4 +3,4 @@
 set PROJECT=%~dp0\IxMilia.Pdf\IxMilia.Pdf.csproj
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --configuration Release %PROJECT% /p:OfficialBuild=true
+dotnet pack --configuration Release %PROJECT%
--- src/create-package.sh
+++ src/create-package.sh
@@ -3,4 +3,4 @@
 _SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 PROJECT=$_SCRIPT_DIR/IxMilia.Pdf/IxMilia.Pdf.csproj
 dotnet restore $PROJECT
-dotnet pack --configuration Release $PROJECT /p:OfficialBuild=true
+dotnet pack --configuration Release $PROJECT
