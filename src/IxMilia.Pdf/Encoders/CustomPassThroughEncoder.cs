662a3cef9bf9f16b68f514730522d6bd5f78b88f
Brett V. Forsgren <brett.forsgren@outlook.com>
2021-06-29 18:32:43 -0600

    add pass through encoder that can specify its own name

---
+++ src/IxMilia.Pdf/Encoders/CustomPassThroughEncoder.cs
@@ -0,0 +1,17 @@
+﻿namespace IxMilia.Pdf.Encoders
+{
+    public class CustomPassThroughEncoder : IPdfEncoder
+    {
+        public string DisplayName { get; private set; }
+
+        public CustomPassThroughEncoder(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public byte[] Encode(byte[] data)
+        {
+            return data;
+        }
+    }
+}
